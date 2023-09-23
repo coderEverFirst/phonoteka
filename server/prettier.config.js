c3cf7513b5919826e3fd44b1bcbe77ee11c85e7e
Oleh Nikulin <oleh.nikulin.dev@gmail.com>
2023-09-23 23:22:14 +0300

    init server configuration

---
+++ server/prettier.config.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-undef */
+module.exports = {
+  printWidth: 100,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  semi: false,
+  arrowParens: 'avoid',
+  endOfLine: 'auto',
+  jest: true,
+}
